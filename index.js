f7258065892cb1ec98387269c3bc5ab386a6f08c
Alessandro Zanardi <alessandro@zanardi.org>
2016-09-23 02:41:57 +0200

    Upgrade to v1.0.1

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 
-// ps: solve this asap!! (30 mins)
+// Fix this asap (30 mins)
 
 // We have received the following e-mail from the support team:
 //
 // Hi techies! One of our clients is complaining about the
-// invoice detail page of our app. He says that the tax of his
-// last invoice appears like 0.020000000000000004 while it should
-// be 0.02. Please, fix this tax calculation and make sure it
-// doesn't occur anymore by extending the tests.
+// billing section of our app. He says that the tax of his
+// last invoice appears to be 0.020000000000000004 while it should
+// be 0.02. Can you please check the tax computation, and make sure
+// we don't get this problem in the future by extending the tests?
 //
-// P.S. solve this asap!!1!one!
+// P.S. Fix this asap
 
-function tax (base) {
+function calcTax (base) {
 
   return base*0.2;
 }
 
-module.exports = tax;
+module.exports = calcTax;
--- test/test.js
+++ test/test.js
@@ -2,14 +2,15 @@
 
 require('chai').should();
 
-var tax = require('../index.js');
+var calcTax = require('../index.js');
 
-describe('tax', function () {
+describe('calcTax', function () {
 
   it('should return the correct amount', function () {
-    tax(20).should.equal(4);
-
+    calcTax(1).should.equal(0.2);
+    calcTax(2).should.equal(0.4);
+    calcTax(3).should.equal(0.6);
+    calcTax(7).should.equal(1.4);
   });
-
 
 });
